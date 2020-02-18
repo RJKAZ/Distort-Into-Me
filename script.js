ea58a774b1fdcfb2549c95321d23953b99c1707d
Richard Kazanowski <rjkaz@comcast.net>
2020-02-18 02:13:40 -0500

    fix

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 function timedSlides() {
   setTimeout(myTimeout1, 2000) 
-  setTimeout(myTimeout2, 2000) 
-  setTimeout(myTimeout3, 2000) 
-  setTimeout(myTimeout4, 2000)
-  setTimeout(myTimeout5, 2000)
-  setTimeout(myTimeout6, 2000)
+  setTimeout(myTimeout2, 8000) 
+  setTimeout(myTimeout3, 12000) 
+  setTimeout(myTimeout4, 15000)
+  setTimeout(myTimeout5, 20000)
+  setTimeout(myTimeout6, 25000)
+  setTimeout(myTimeout7, 30000)
   }
   function myTimeout1() {
-    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/slide1.jpg\'>';
+    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/1.jpg\'>';
   }
   function myTimeout2() {
-    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/slide2.jpg\'>';
+    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/2.jpg\'>';
   }
   function myTimeout3() {
-    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/slide3.jpg\'>';
+    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/3.jpg\'>';
   }
   function myTimeout4() {
-    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/slide4.jpg\'>';
+    document.getElementById("demo").innerHTML = '<img class="mySlides w3-animate-fading" style="width:50%" style="height:50%" src=\'Assets/art/4.jpg\'>';
   }
   function myTimeout5() {
-    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/slide5.jpg\'>';
+    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/5.jpg\'>';
   }
   function myTimeout6() {
-    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/slide6.jpg\'>';
+    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/6.jpg\'>';
   }
   function myTimeout7() {
-    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/slide7.jpg\'>';
+    document.getElementById("demo").innerHTML = '<img class="slides" src=\'Assets/art/7.jpg\'>';
   };
 
 
